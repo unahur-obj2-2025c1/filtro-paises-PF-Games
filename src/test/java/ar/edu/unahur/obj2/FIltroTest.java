f7f9398c79ee43e2726508e9a9b87b3e3083978e
PFGames <132846673+PF-Games@users.noreply.github.com>
2025-05-05 16:49:57 -0300

    Prueba filtro árabe

--- src/test/java/ar/edu/unahur/obj2/FIltroTest.java
+++ src/test/java/ar/edu/unahur/obj2/FIltroTest.java
@@ -1,6 +1,8 @@
 package ar.edu.unahur.obj2;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+
+import ar.edu.unahur.obj2.filters.FiltroIdioma;
 import ar.edu.unahur.obj2.filters.IFiltro;
 import ar.edu.unahur.obj2.model.Pais;
 import ar.edu.unahur.obj2.service.IPaisService;
@@ -13,7 +15,11 @@ public class FIltroTest {
 	void prueba() {
 		IPaisService paisService = new PaisService();
         List<Pais> paises = paisService.getPaises();
-       
-    
-}
+        IFiltro filtro = new FiltroIdioma("Arabic");
+        
+        var paisesFiltrados = paises.stream()
+        				.filter(p -> filtro.apply(p))
+        				.toList();
+        System.out.println(paisesFiltrados);
+    }
 }
